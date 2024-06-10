1cc3cfa30c34fb5a747336b5893c99caf098c82d
rylenlobo <90313517+rylenlobo@users.noreply.github.com>
2024-06-10 19:53:35 +0530

    chore: Add react-device-detect dependency for mobile device detection

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Slider from "./components/Slider"
 import Slide from "./components/Slide";
 import NextAndProgress from "./components/NextAndProgress";
 import dslogo from "./assets/pngwing.com.png"
-import { motion } from "framer-motion";
-
+import { Computer } from 'lucide-react';
+import { isMobile } from 'react-device-detect';
 
 
 const App = () => {
@@ -21,9 +21,19 @@ const App = () => {
     },
   };
 
+  if (isMobile) {
+    return <>
+      <div className="h-screen gap-10 w-screen flex flex-col justify-center items-center">
+        <Computer size={50} />
+        <p>Please use this on a desktop.</p>
+      </div>
+    </>
+  }
+
+
+
   return (
     <>
-
       <div className="relative h-screen w-screen overflow-hidden  bg-stone-800">
         <div className="absolute z-50 top-0 w-full h-1">
           {/* <motion.div style={{ originX: 0 }} variants={timerVariants} initial="initial" animate="animate" exit="exit" className="h-full bg-red-500" /> */}
